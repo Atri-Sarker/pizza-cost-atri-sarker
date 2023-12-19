9048634bba65070f1556668df85e7eb10c223704
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2023-12-19 11:46:00 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+let total = 0
+let tax = 0
+let subtotal = 0
+let diameter = 0
+game.splash("Welcome to Atri's Pizzeria!")
+let RENT_COST = 1
+let LABOUR_COST = 0.75
+let MATERIAL_COST = 0.5
+let HST = 13 / 100
+while (true) {
+    diameter = game.askForNumber("Enter the pizza's diameter ( inches ):")
+    subtotal = LABOUR_COST + RENT_COST + MATERIAL_COST * diameter
+    tax = subtotal * HST
+    total = Math.floor((subtotal + tax) / 0.01) / 100
+    game.splash("The total cost of the pizza is $" + total + "!")
+    game.splash("Another Pizza?")
+}
